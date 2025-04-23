691b52e15c936c1387dd11ba15ed9454884763b9
Cassycasss <165082685+Cassycasss@users.noreply.github.com>
2025-04-23 05:34:13 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import HomePage   from './components/Home';
-import QuizPage   from './components/Quiz';       
-import Resources  from './components/Resources';
-import Navbar     from './components/Navbar';
+import Navbar    from './components/Navbar';
+import HomePage  from './components/Home';
+import Resources from './components/Resources';
+import QuizPage  from './components/Quiz';
 
 export default function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/"          element={<HomePage />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/quiz"      element={<QuizPage />} />
-        
       </Routes>
-    </Router>
+    </>
   );
 }
+
