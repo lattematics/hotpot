8ec3fac817549742073bef38aa6801998cf6dcd5
Cassycasss <165082685+Cassycasss@users.noreply.github.com>
2025-04-23 05:53:39 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './style.css';                              
+import './index.css';
+import './style.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import App from './App.jsx';
