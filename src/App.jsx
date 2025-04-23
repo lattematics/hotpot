4ea2dad786a245e48ff24a315839b607a780dd29
Cassycasss <165082685+Cassycasss@users.noreply.github.com>
2025-04-23 04:21:36 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import HomePage from './components/Home';
-import Navbar from './components/Navbar';
-import QuizPage from './components/Quiz';
-import QuizQuestion from './components/QuizQuestion';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/Home';
+import QuizPage from './components/QuizPage';
+import Navbar from './components/Navbar';
+import Resources from './components/Resources';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/quiz" element={<QuizPage />} />
-          <Route path="/quiz/start" element={<QuizQuestion/>} />
-          {/* Add other routes here like /quiz, /forum, etc. */}
-        </Routes>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/resources" element={<Resources />} />
+      </Routes>
     </Router>
-    </div>
   );
 }
 
