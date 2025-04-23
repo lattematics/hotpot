3d5b0fb373208b29ce00281f9a2a552f47cc3bfd
Cassycasss <165082685+Cassycasss@users.noreply.github.com>
2025-04-23 04:25:10 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-    return (
-        <header className="header">
-          <div className="logo">HealthUs</div>
-          <nav className="nav">
-            <Link to="/">Home</Link>
-            <Link to="/resources">Resources</Link>
-            <Link to="/quiz">Quiz</Link>
-            <Link to="/forum">Forum</Link>
-            <Link to="/about">About</Link>
-            <Link to="/about">Login</Link>
-          </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <div className="logo">HealthUs</div>
+      <nav className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/resources">Resources</Link>
+        <Link to="/quiz">Quiz</Link>
+        <Link to="/forum">Forum</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+    </header>
+  );
 }
