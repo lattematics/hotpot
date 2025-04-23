b693dbc1d7480b51ca9ff46853e87eb150530aa0
Cassycasss <165082685+Cassycasss@users.noreply.github.com>
2025-04-23 05:04:11 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/Home';
-import QuizPage from './components/QuizPage';
-import Navbar from './components/Navbar';
-import Resources from './components/Resources';
+
+import HomePage     from './components/Home';
+import Resources    from './components/Resources';      
+import QuizPage     from './components/Quiz';
+import QuizQuestion from './components/QuizQuestion';
+import ForumPage    from './components/Forum';
+import AboutPage    from './components/About';
+import LoginPage    from './components/Login';
+
+import Navbar       from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-        <Route path="/resources" element={<Resources />} />
+        <Route path="/"            element={<HomePage />} />
+        <Route path="/resources"   element={<Resources />} />    
+        <Route path="/quiz"        element={<QuizPage />} />
+        <Route path="/quiz/start"  element={<QuizQuestion />} />
+        <Route path="/forum"       element={<ForumPage />} />
+        <Route path="/about"       element={<AboutPage />} />
+        <Route path="/login"       element={<LoginPage />} />
       </Routes>
     </Router>
   );
