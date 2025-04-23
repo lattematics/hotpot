b24f6f6dcbb34df8800354ff5aee2a5a2314be56
Cassycasss <165082685+Cassycasss@users.noreply.github.com>
2025-04-23 05:17:45 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import './style.css';
-
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import App from './App.jsx';
@@ -12,7 +11,6 @@ import App from './App.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
     <BrowserRouter>
       <App />
     </BrowserRouter>
