7ed08859bf869c334690b190dda65f10a92ddbfa
Linda Giang <97063743+lattematics@users.noreply.github.com>
2025-05-19 02:29:34 -0700

    small edits

--- src/components/LoginSignup.jsx
+++ src/components/LoginSignup.jsx
@@ -1,7 +1,7 @@
 import Login from './Login';
 import Signup from './Signup';
 
-export default function Navbar() {
+export default function LoginSignup() {
   return (
     <div class="logform">
         <Signup />
